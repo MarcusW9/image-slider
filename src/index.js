b4264140adaf96ff6acb8db7068e71e07e788388
Marcus W <marcuswsywong@gmail.com>
2024-04-18 19:34:44 +0100

    Add slider
    Package setup
    Add slider functionality with 3 images

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import _ from "lodash";
+// import _ from "lodash";
 import "./style.css";
+import img1 from './images/bailey-zindel-NRQV-hBF10M-unsplash.jpg';
+import img2 from './images/mark-harpur-K2s_YE031CA-unsplash.jpg';
+import img3 from './images/pine-watt-2Hzmz15wGik-unsplash.jpg';
 
-console.log("hello world");
+let number = 0;
 
-const name = "James";
+function updateActiveImage() {
+    document.querySelectorAll(".active").forEach((activeElement) => activeElement.classList.remove("active"))
+    document.querySelectorAll(".image")[number].classList.add("active");
 
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
+    document.querySelectorAll(".navigation-dot")[number].classList.add("active");
 }
 
-document.body.appendChild(component());
+document.querySelectorAll(".slider-button").forEach((sliderButton) => {
+    if (sliderButton.classList.contains("slider-left")) {
+            sliderButton.addEventListener(("click"), () => {
+                console.log("left")
+                if (number <= 0) return
+                    number -= 1
+                    updateActiveImage();
+        }) 
+    }
+    
+    if (sliderButton.classList.contains("slider-right")) {
+        sliderButton.addEventListener(("click"), () => {
+            console.log("right")
+            if (number >= 2) return
+                number += 1
+                updateActiveImage();
+        }) 
+    }
+})
+
+
+
