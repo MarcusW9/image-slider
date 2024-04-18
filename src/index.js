35ef79d09abb82e7767251c30c40aef286ec97ca
Marcus W <marcuswsywong@gmail.com>
2024-04-18 20:06:19 +0100

    Add animation and navigation buttons
    Fade in animation for each image when changing
    Clicking on the dots will navigate to respective image

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import img1 from './images/bailey-zindel-NRQV-hBF10M-unsplash.jpg';
 import img2 from './images/mark-harpur-K2s_YE031CA-unsplash.jpg';
 import img3 from './images/pine-watt-2Hzmz15wGik-unsplash.jpg';
 
-let number = 0;
+let imageNumber = 0;
 
-function updateActiveImage() {
+function updateActiveImage(number) {
     document.querySelectorAll(".active").forEach((activeElement) => activeElement.classList.remove("active"))
     document.querySelectorAll(".image")[number].classList.add("active");
 
@@ -16,22 +16,26 @@ function updateActiveImage() {
 document.querySelectorAll(".slider-button").forEach((sliderButton) => {
     if (sliderButton.classList.contains("slider-left")) {
             sliderButton.addEventListener(("click"), () => {
-                console.log("left")
-                if (number <= 0) return
-                    number -= 1
-                    updateActiveImage();
+                if (imageNumber <= 0) return
+                    imageNumber -= 1
+                    updateActiveImage(imageNumber);
         }) 
     }
     
     if (sliderButton.classList.contains("slider-right")) {
         sliderButton.addEventListener(("click"), () => {
-            console.log("right")
-            if (number >= 2) return
-                number += 1
-                updateActiveImage();
+            if (imageNumber >= 2) return
+                imageNumber += 1
+                updateActiveImage(imageNumber);
         }) 
     }
 })
 
+document.querySelectorAll(".navigation-button").forEach((navigationButton, index) => {
+    navigationButton.addEventListener("click", () => {
+        imageNumber = index
+        updateActiveImage(index)
+    })
+})
 
 
